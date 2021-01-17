a5d1354c54b1cefeb258c2512a38d3b44869173c
Kristen Groller <groller@rogers.com>
2021-01-16 20:53:49 -0500

    language buttons added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,8 +1,40 @@
-
+var languageButtonsEl = document.querySelector("#language-buttons");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter Github username");
+    }
+};
 
 
+
+
+var getFeaturedRepos = function(language) {
+var apiUrl = "https://api.github.com/search/repositories?q=" + language + "=is:featured&sort=help-wanted-issues";
+    
+
+fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+        response.json().then(function(data) {
+            displayRepos(data.items, language);
+             });
+    } else {
+        alert("Error: " + response.statusText);
+    }
+});
+};
 
 var getUserRepos = function(user) {
 
@@ -29,22 +61,6 @@ var getUserRepos = function(user) {
 }; 
    
 
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-
-    //get value from input element
-    var username = nameInputEl.value.trim();
-
-    if (username) {
-        getUserRepos(username);
-        nameInputEl.value = "";
-    } else {
-        alert("Please enter Github username");
-    }
-    console.log(event);
-};
 
 var displayRepos = function(repos, searchTerm) {
     repoContainerEl.textContent = "";
@@ -89,10 +105,7 @@ var displayRepos = function(repos, searchTerm) {
     }
 
     // append to container
-    repoEl.appendChild(statusEl);
-
-        
-
+    repoEl.appendChild(statusEl);       
 
         //append ccontainer to dom
         repoContainerEl.appendChild(repoEl);
@@ -100,5 +113,16 @@ var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
 };
+var buttonClickHandler = function(event) {
+var language = event.target.getAttribute("data-language");
 
+if (language) {
+    getFeaturedRepos(language);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+}
+
+};
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
